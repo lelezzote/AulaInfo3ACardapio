444594959cf3e75cdaff25e0f59ef9704d988447
lelezzote <159335824+lelezzote@users.noreply.github.com>
2024-06-25 18:24:49 -0300

    construçaoDeTresStados

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
 import "../src/globals.css";
-
+import {useState} from "react"
 
 export default function App() {
-
-   
-
-
+     const [nome, setNome] = useState("Professor");
+      const mudarNome = () => {
+        setNome("Joao");
+      }
+      const[numero, setNumero] = useState("455");
+      const mudarNumero = () => {
+        setNumero("467");
+    }
+    const[cpf, setCpf] = useState("68472894637");
+        
+    const mudarCpf = () => {
+        setCpf("648988989");
+    }
     return (
        <div>
-
+           {nome}
+            {numero}
+            {cpf}
+            <button onClick = {mudarNome}>Mudar</button>
+            <button onClick = {mudarNumero}>Mudar</button>
+            <button onClick = {mudarCpf}>Mudar</button>
        </div>
     );
 }
